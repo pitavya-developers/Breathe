e69b7aac63ffc9b33dc3b98906e09cdddcfac0b4
faiz-pitavya <faizrocks9211@gmail.com>
2020-08-08 23:45:37 +0530

    Save User Api Completed , tested

--- api/breathe/src/main/java/com/pitavya/breathe/controllers/LoginController.java
+++ api/breathe/src/main/java/com/pitavya/breathe/controllers/LoginController.java
@@ -1,14 +1,28 @@
 package com.pitavya.breathe.controllers;
 
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pitavya.breathe.model.User;
+import com.pitavya.breathe.requestModels.SaveUserRequestModel;
+import com.pitavya.breathe.service.SaveUserService;
+
 @RestController
+@RequestMapping("/breathe/api")
 public class LoginController {
 
-	@RequestMapping(value = "/test")
-	public String testApi() {
-		return "Test SuccessFull for Pitavya-Breathe Api";
+	private static Logger LOGGER = Logger.getLogger(LoginController.class);
+
+	@Autowired
+	private SaveUserService saveUserService;
+
+	@RequestMapping(value = "/signin")
+	public User saveUser(@RequestBody SaveUserRequestModel requestModel) {
+		LOGGER.info("saveUser()");
+		return saveUserService.saveUserService(requestModel);
 	}
 
 }
